359addf94876f9c232b9192d975f7626d792134c
sailorlyx <79230737+sailorlyx@users.noreply.github.com>
2021-03-25 21:13:41 +0100

    probando

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,7 +14,7 @@ import { FooterComponent } from './footer/footer.component';
     HomeComponent,
     HeaderComponent,
     MainComponent,
-    FooterComponent
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
