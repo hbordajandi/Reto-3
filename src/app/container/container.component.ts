4604ed093fe86c75f9fd7bcc73f8434cb263cc73
Héctor Bordajandi Moleón <hbordajandi@alfatecsistemas.es>
2021-03-27 23:31:52 +0100

    Implementados contenedores tareas.

--- src/app/container/container.component.ts
+++ src/app/container/container.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-container',
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./container.component.scss']
 })
 export class ContainerComponent implements OnInit {
-
+  @Input() public title: string = 'Mi lista';
+  @Input() public imgSrc: string = '';
   constructor() { }
 
   ngOnInit(): void {
